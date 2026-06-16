7f7c3ebd6796a418dba8ac8049ff119a1824c9f4
Siddharth <sidp5276@gmail.com>
2026-06-16 22:39:44 +0530

    Chat completion Request

---
+++ src/main/java/com/APIFramework/pojos/Groq/requestPOJO/ChatCompletionRequest.java
@@ -0,0 +1,27 @@
+package com.APIFramework.pojos.Groq.requestPOJO;
+
+import com.google.gson.annotations.Expose;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatCompletionRequest {
+
+    @Expose
+    private String model;
+    @Expose
+    private List<Message> messages;
+
+    public ChatCompletionRequest() {
+        this.messages = new ArrayList<>();
+    }
+
+    public String getModel() { return model; }
+    public void setModel(String model) { this.model = model; }
+
+    public List<Message> getMessages() { return messages; }
+    public void setMessages(List<Message> messages) { this.messages = messages; }
+
+    public void addMessage(Message m) { this.messages.add(m);
+    }
+}
