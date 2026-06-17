c67db7db8fc6f264baf5c22fccac24352ad8f73e
Siddharth <sidp5276@gmail.com>
2026-06-17 12:04:10 +0530

    Invalid token response

---
+++ src/main/java/com/APIFramework/pojos/restfulbook/responsepojo/InvalidTokenResponse.java
@@ -0,0 +1,13 @@
+package com.APIFramework.pojos.restfulbook.responsepojo;
+
+public class InvalidTokenResponse {
+    private String reason;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
